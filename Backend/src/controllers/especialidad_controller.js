f5390121d5177254902213d18d0bec7125727c9a
David Vela <david.vela@epn.edu.ec>
2024-09-02 11:54:46 -0500

    Mejoras y mas funciones

--- Backend/src/controllers/especialidad_controller.js
+++ Backend/src/controllers/especialidad_controller.js
@@ -1,4 +1,5 @@
 import especialidades from "../models/especialidades.js";
+import mongoose from "mongoose";
 
 const registrarEspecialidad=async(req,res)=>{
     const {codigo,nombre,descripcion}=req.body
@@ -13,7 +14,44 @@ const registrarEspecialidad=async(req,res)=>{
     .json({ msg: "Se acaba de registrar una especialidad en el sistema " });
 
 }
+const listarEspecialidades = async (req,res)=>{
+    if (req.especialidadesBDD) {
+        const especialidad = await especialidades.find(req.especialidadesBDD.id)
+        res.status(200).json(especialidad)
+    }
+    else{
+        const especialidad = await especialidades.find({estado:true})
+        res.status(200).json(especialidad)
+    }
+}
+const actualizarEspecialidad=async(req,res)=>{
+    const { id } = req.params;
+  
+    if (Object.values(req.body).includes(""))
+      return res
+        .status(400)
+        .json({ msg: "Lo sentimos, debes llenar todos los campos" });
+  
+    if (!mongoose.Types.ObjectId.isValid(id))
+      return res
+        .status(404)
+        .json({ msg: `Lo sentimos, no se encontró la especialidad ${id} en el centro ` });
+  
+    await especialidades.findByIdAndUpdate(req.params.id, req.body);
+  
+    res.status(200).json({ msg: "Actualización exitosa de la especialidad" });
+}
+const eliminarEspecialidad = async(req,res)=>{
+    const {id} = req.params
+    if( !mongoose.Types.ObjectId.isValid(id) ) return res.status(404).json({msg:`La especialidad no existe `})
+    await especialidades.findByIdAndDelete(req.params.id)
+    res.status(200).json({msg:"Se ha eliminado la especialidad de la base de datos "})
+}
+
 
 export {
-    registrarEspecialidad
+    registrarEspecialidad,
+    listarEspecialidades,
+    actualizarEspecialidad,
+    eliminarEspecialidad
 }
--- Backend/src/routers/especialidad_routes.js
+++ Backend/src/routers/especialidad_routes.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { registrarEspecialidad } from "../controllers/especialidad_controller.js";
+import { actualizarEspecialidad, eliminarEspecialidad, listarEspecialidades, registrarEspecialidad } from "../controllers/especialidad_controller.js";
 
 const router=Router()
 
 router.post('/especialidad/registro',registrarEspecialidad)
+router.get('/especialidad/listar',listarEspecialidades)
+router.put('/especialidad/actualizar/:id',actualizarEspecialidad)
+router.delete('/especialidad/eliminar/:id',eliminarEspecialidad)
 
 export default router
